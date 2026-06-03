a72328008af290ed0a86e18716dce8e886306a29
yassineAk1 <yassine9a6@gmail.com>
2026-06-03 11:18:06 +0200

    #10 server opgezet

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express, { response } from 'express'
+import { Liquid } from 'liquidjs';
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static('public'))
+
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+app.set('views', './views')
+
+
+app.set('port', process.env.PORT || 8000)
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
